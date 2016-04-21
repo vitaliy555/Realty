60606f20e9c9051bc405be9ea8f516fa276772ef
Vitalii Chechel <vitalii_chechel@epam.com>
2016-04-21 18:43:25 +0300

    add suggest city class

---
+++ src/main/java/com/realty/dto/MainTryTestSomeFeatures.java
@@ -0,0 +1,9 @@
+package com.realty.dto;
+
+import java.io.IOException;
+
+public class MainTryTestSomeFeatures {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
---
+++ src/main/java/com/realty/dto/autosuggest/CitySuggest.java
@@ -0,0 +1,5 @@
+package com.realty.dto.autosuggest;
+
+public class CitySuggest {
+    public static final String CITY_SUGGEST_URL="olx.ua/ajax/geo6/autosuggest/";
+}
