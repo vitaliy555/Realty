cff7f374470064ed08f589211eee6737f0260725
Vitalii Chechel <vitalii_chechel@epam.com>
2016-04-22 18:23:01 +0300

    added city suggest class

--- src/main/java/com/realty/dto/autosuggest/CitySuggest.java
+++ src/main/java/com/realty/dto/autosuggest/CitySuggest.java
@@ -1,5 +1,14 @@
 package com.realty.dto.autosuggest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
 public class CitySuggest {
-    public static final String CITY_SUGGEST_URL="olx.ua/ajax/geo6/autosuggest/";
+    private static final String CITY_SUGGEST_URL = "olx.ua/ajax/geo6/autosuggest/";
+    @Autowired
+    private RestTemplate rest;
+
+    public void findCities(){
+        rest.postForEntity(CITY_SUGGEST_URL,/)
+    }
 }
