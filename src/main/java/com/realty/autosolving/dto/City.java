f5ec1ac4f39c81626e307f06782f98f90115a1f2
Vitalii Chechel <vitalii_chechel@epam.com>
2016-05-05 18:13:23 +0300

    add logger

--- src/main/java/com/realty/autosolving/dto/City.java
+++ src/main/java/com/realty/autosolving/dto/City.java
@@ -2,9 +2,11 @@ package com.realty.autosolving.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 public class City {
     private int id;
     private String text;
--- src/main/java/com/realty/autosolving/services/CityFindService.java
+++ src/main/java/com/realty/autosolving/services/CityFindService.java
@@ -1,20 +1,21 @@
 package com.realty.autosolving.services;
 
-import com.google.common.collect.Lists;
-import com.realty.autosolving.dto.City;
-import com.realty.autosolving.dto.CityList;
-import com.realty.autosolving.errors.CitySuggestAPIException;
+import static com.realty.autosolving.AlphabetHelper.getAllCombination;
+import static com.realty.autosolving.services.OLXResponceSatus.ok;
+
+import java.util.List;
+
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
-import static com.realty.autosolving.AlphabetHelper.getAllCombination;
-import static com.realty.autosolving.services.OLXResponceSatus.ok;
+import com.google.common.collect.Lists;
+import com.realty.autosolving.dto.City;
+import com.realty.autosolving.dto.CityList;
 
 /**
  * Created by vitaliy on 4/29/2016.
@@ -50,13 +51,13 @@ public class CityFindService {
         MultiValueMap<String, Object> form = new LinkedMultiValueMap<String, Object>();
         form.add("data", phrase);
         CityList cityList = rest.postForObject(cityUrl, form, CityList.class);
-        if (!ok.equals(cityList.getStatus())) {
-            log.error(String.format("Request: %s \nResponse: %s", form,cityList));
-            throw new CitySuggestAPIException("Bad response: " + cityList.getStatus().name());
+        List<City> result = Lists.newArrayList();
+        if (ok.equals(cityList.getStatus())) {
+            result = cityList.getListOfCities();
         } else {
-            return cityList.getListOfCities();
+            log.debug(String.format("Request: %s \nResponse: %s", form, cityList));
         }
+        return result;
     }
-
 
 }
